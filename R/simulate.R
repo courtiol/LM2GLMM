1007d72b86024fb062fe80764b0886e8620af90a
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-04-17 14:38:47 +0200

    new function to simulate Aliens

--- R/simulate.R
+++ R/simulate.R
@@ -21,3 +21,25 @@ simulate_predictors_1C1G <- function(n, k){
   rownames(ordered_result) <- NULL
   return(ordered_result)
 }
+
+#' Function simulating the Alien datasets
+#'
+#' The Aliens considered here have a size that is varies linearly according to
+#' the number of humans they have eaten.
+#'
+#' @param N the number of Aliens (default = 12)
+#' @param intercept the value for the intercep (default = 50)
+#' @param slope the value for the slope (default = 1.5)
+#' @param sigma2 the value for the error variance (default = 25)
+#'
+#' @return a \var{data.frame} containing the simulated data
+#' @export
+#'
+#' @examples
+#' simulate_Aliens()
+#'
+simulate_Aliens <- function(N = 12, intercept = 50, slope = 1.5, sigma2 = 25) {
+  Alien <- data.frame(humans_eaten = sample(1:N))
+  Alien$size <- rnorm(n = N, mean = intercept + slope*Alien$humans_eaten, sd = sqrt(sigma2))
+  return(Alien)
+}
