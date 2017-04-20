c9ef7e4545a7bd75fcfa82e7a4f657ace7b3abdb
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-04-20 16:26:02 +0200

    add overall seed

--- inst/doc/LM_assumptions.R
+++ inst/doc/LM_assumptions.R
@@ -3,6 +3,7 @@ library(LM2GLMM)
 library(lmtest)
 knitr::opts_chunk$set(cache = FALSE, cache.path = "./cache_knitr/LM_assumptions/", fig.path = "./fig_knitr/LM_assumptions/", fig.align = "center", fig.width = 4, fig.height = 4)
 options(width = 200)
+set.seed(1L)
 
 ## ----simple non linear--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 set.seed(1L)
