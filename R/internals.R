55f34fc7bd033796670179b30c4fae69cbf1d597
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-04-26 21:12:35 +0200

    reintroduce browseVignettes, which seems to work!

--- R/internals.R
+++ R/internals.R
@@ -2,14 +2,17 @@
   packageStartupMessage(
     "\n #############################################",
     "\n #                                           #",
-    "\n #     This is the package for the course    #",
+    "\n #    This is the package for the course     #",
     "\n #                                           #",
     "\n #    Advanced Statistical Applications:     #",
     "\n #          from LM to GLMM using R          #",
     "\n #                                           #",
     "\n #   You have the version ", utils::packageDescription("LM2GLMM")$Version," installed!   #",
     "\n #                                           #",
-    "\n # Type get_vignettes() to access the slides #",
+    "\n #    To access the slides, type either      #",
+    "\n #   browseVignettes(package = 'LM2GLMM')    #",
+    "\n #                    or                     #",
+    "\n #             get_vignettes()               #",
     "\n #                                           #",
     "\n #############################################", "\n")
 }
