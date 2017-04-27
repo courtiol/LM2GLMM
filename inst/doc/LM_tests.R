0531110ef15425ef5874357f9b72232d19dac157
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-04-27 17:40:57 +0200

    rebuild vignette for new version

--- inst/doc/LM_tests.R
+++ inst/doc/LM_tests.R
@@ -7,3 +7,8 @@ library(rgl)
 library(car)
 knitr::opts_chunk$set(cache = TRUE, cache.path = "./cache_knitr/LM_tests/", fig.path = "./fig_knitr/LM_tests/")
 
+## ----non nested AIC, cache = FALSE---------------------------------------
+AIC(mod_UK1)
+AIC(mod_UK5)
+exp((AIC(mod_UK5) - AIC(mod_UK1)) / 2) ## evidence ratio
+
