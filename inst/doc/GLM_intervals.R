57f39d05eca387e3d04d508a8f32f320a9166078
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-05-08 15:26:50 +0200

    reoxygenize

--- inst/doc/GLM_intervals.R
+++ inst/doc/GLM_intervals.R
@@ -6,3 +6,9 @@ library(spaMM)
 options(width = 120)
 knitr::opts_chunk$set(cache = TRUE, cache.path = "./cache_knitr/GLM_uncertainty/", fig.path = "./fig_knitr/GLM_uncertainty/", fig.width = 4, fig.height = 4, fig.align = "center")
 
+## ---- cache = FALSE---------------------------------------------------------------------------------------------------
+summary(mod_binar2)$coef
+z <- (mod_binar2$coef - 0) / sqrt(diag(vcov(mod_binar2)))
+pvalues <- 2 * (1 - pnorm(abs(z)))
+cbind(z, pvalues)
+
