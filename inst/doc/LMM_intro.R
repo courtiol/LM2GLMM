7d88039802133c2b4ced71d4de86d7dd6a89ab70
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-05-17 01:14:37 +0200

    reoxygenise

--- inst/doc/LMM_intro.R
+++ inst/doc/LMM_intro.R
@@ -7,28 +7,25 @@ options(width = 100)
 knitr::opts_chunk$set(cache = TRUE, cache.path = "./cache_knitr/LMM_intro/", fig.path = "./fig_knitr/LMM_intro/", fig.width = 5, fig.height = 5, fig.align = "center")
 
 ## ---- cache = FALSE-------------------------------------------------------------------------------
-Aliens
+c(mod$I2, mod$H2, mod$QE) ## from metafor
 
-## ---- cache = FALSE-------------------------------------------------------------------------------
-library(lme4)
-(mod_lme4 <- lmer(y ~ x + (1|group), data = Aliens, REML = FALSE))
-c(var.group = as.numeric(attr(VarCorr(mod_lme4)$group, "stddev")^2), var.error = as.numeric(attr(VarCorr(mod_lme4), "sc")^2))
+get_meta_metrics <- function(model, vi) {
+  wi <- 1/vi  ## weights = inverse of sampling variance
+  k <- length(model$ranef)  ## number of groups in random term (here number of studies)
+  p <- length(model$fixef)  ## number of fixed effect parameters
+  W <- diag(wi, nrow = k, ncol = k)  ## matrix of weights
+  X <- as.matrix(as.data.frame(model$X.pv))  ## design matrix
+  stXWX <- solve(t(X) %*% W %*% X)  ## weighted vcov of estimates
+  P <- W - W %*% X %*% stXWX %*% crossprod(X, W)  ## weighted vcov of ??
+  vi.avg <- (k - p) / sum(diag(P))
+  I2 <- as.numeric(100 * model$lambda / (vi.avg + model$lambda) )
+  H2 <- as.numeric((vi.avg + model$lambda) / vi.avg)
+  QE <- max(0, c(crossprod(model$y, P) %*% model$y))
+  return(c(I2 = I2, H2 = H2, QE = QE, vi.avg = vi.avg))
+}
 
-## ---- cache = FALSE-------------------------------------------------------------------------------
-library(spaMM)
-(mod_spaMM <- fitme(y ~ x + (1|group), data = Aliens, method = "ML"))
+get_meta_metrics(mod_spaMM, dat.bcg$sampling.var) ## add vi.avg: weighted mean within study sampling variance
 
-## ---- cache = FALSE-------------------------------------------------------------------------------
-plot(ecdf(test))
-abline(0, 1, col = "red")
-
-## ---- cache = FALSE-------------------------------------------------------------------------------
-plot(ecdf(test2))
-abline(0, 1, col = "red")
-
-## ---- cache = FALSE-------------------------------------------------------------------------------
-mod2
-
-## ---- cache = FALSE-------------------------------------------------------------------------------
-as.data.frame(ranef(mod2))
+## ---- fig.width = 6, fig.height = 5.5, cache = FALSE----------------------------------------------
+metafor::plot.rma.uni(mod)  ## or just plot(mod)
 
