d0547d0510952e0d0882d52ab5b27aac8cac32c1
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-05-16 08:59:58 +0200

    reoxygenize

--- inst/doc/LMM_intro.R
+++ inst/doc/LMM_intro.R
@@ -6,3 +6,29 @@ library(car)
 options(width = 100)
 knitr::opts_chunk$set(cache = TRUE, cache.path = "./cache_knitr/LMM_intro/", fig.path = "./fig_knitr/LMM_intro/", fig.width = 5, fig.height = 5, fig.align = "center")
 
+## ---- cache = FALSE-------------------------------------------------------------------------------
+Aliens
+
+## ---- cache = FALSE-------------------------------------------------------------------------------
+library(lme4)
+(mod <- lmer(y ~ x + (1|group), data = Aliens, REML = FALSE))
+c(var.group = as.numeric(attr(VarCorr(mod)$group, "stddev")^2), var.error = as.numeric(attr(VarCorr(mod), "sc")^2))
+
+## ---- cache = FALSE-------------------------------------------------------------------------------
+library(spaMM)
+(mod <- fitme(y ~ x + (1|group), data = Aliens, method = "ML"))
+
+## ---- cache = FALSE-------------------------------------------------------------------------------
+plot(ecdf(test))
+abline(0, 1, col = "red")
+
+## ---- cache = FALSE-------------------------------------------------------------------------------
+plot(ecdf(test2))
+abline(0, 1, col = "red")
+
+## ---- cache = FALSE-------------------------------------------------------------------------------
+mod2
+
+## ---- cache = FALSE-------------------------------------------------------------------------------
+as.data.frame(ranef(mod2))
+
