d8260157850db71fe80f3f52af93e82e7c922899
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2017-05-19 08:16:35 +0200

    reoxygenize

---
+++ inst/doc/LMM_more.R
@@ -0,0 +1,7 @@
+## ----setup, include=FALSE-------------------------------------------------------------------------
+library(LM2GLMM)
+library(spaMM)
+library(lme4)
+options(width = 100)
+knitr::opts_chunk$set(cache = TRUE, cache.path = "./cache_knitr/LMM_more/", fig.path = "./fig_knitr/LMM_more/", fig.width = 5, fig.height = 5, fig.align = "center")
+
