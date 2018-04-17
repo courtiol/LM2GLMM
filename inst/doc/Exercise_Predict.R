63eb1a50885d705e01de62fa637e6fab8fe36658
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-04-17 22:59:03 +0200

    rebuild

--- inst/doc/Exercise_Predict.R
+++ inst/doc/Exercise_Predict.R
@@ -1,5 +1,6 @@
 ## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(cache = FALSE, fig.align = "center", fig.width = 5, fig.height = 5)
+knitr::opts_chunk$set(cache = TRUE, fig.align = "center", fig.width = 5, fig.height = 5,
+                      cache.path = "./cache_knitr/Exo_pred/", fig.path = "./fig_knitr/Exo_pred/")
 
 ## ------------------------------------------------------------------------
 library(LM2GLMM) ## load the package
--- inst/doc/LM_tests.R
+++ inst/doc/LM_tests.R
@@ -9,3 +9,12 @@ library(car)
 spaMM.options(nb_cores = 4L)
 knitr::opts_chunk$set(cache = TRUE, cache.path = "./cache_knitr/LM_tests/", fig.path = "./fig_knitr/LM_tests/")
 
+## ----ghlt plot, fig.align = "center", fig.width = 8, fig.height = 4---------------------------------------------------
+par(oma = c(0, 15, 0, 0))  ## increase left outer margin
+plot(glht(mod_drink, linfct = mcp(drink = "Tukey")))
+
+## ----drink------------------------------------------------------------------------------------------------------------
+UK$drink2 <- relevel(UK$drink, ref = "Not at all")
+summary(mod_drink2 <- lm(height ~ drink2, data = UK))$coef
+## compare to summary(glht(mod_drink2, linfct = mcp(drink2 = "Dunnett")))
+
