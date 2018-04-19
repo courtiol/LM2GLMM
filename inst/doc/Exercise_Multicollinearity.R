32567d1c0850a05eb463b649eb14bd6bcbf926c7
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2018-04-19 18:01:12 +0200

    rebuild

--- inst/doc/Exercise_Multicollinearity.R
+++ inst/doc/Exercise_Multicollinearity.R
@@ -1,5 +1,6 @@
 ## ----setup, include=FALSE------------------------------------------------
 library(LM2GLMM)
 knitr::opts_chunk$set(fig.align = "center", fig.width = 5, fig.height = 5,
-                      cache = TRUE, cache.path = "./cache_knitr/Exo_multicollinearity/", fig.path = "./fig_knitr/Exo_multicollinearity/")
+                      cache = TRUE, cache.path = "./cache_knitr/Exo_multicol/",
+                      fig.path = "./fig_knitr/Exo_multicol/")
 
