f99153ff5157160c45f5282d3747c56642ce4283
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-04-18 15:32:01 +0200

    Tweak internal functions

--- R/zzz.R
+++ R/zzz.R
@@ -19,16 +19,21 @@
 
 
 .build_vignettes <- function() {
+  #devtools::build_vignettes(pkg = ".", dependencies = "VignetteBuilder", clean = FALSE,
+  #                          upgrade = "never", quiet = FALSE, install = TRUE,
+  #                          keep_md = TRUE)
   pkg <- devtools::as.package(".")
-  devtools::install_deps(pkg, "VignetteBuilder", upgrade = TRUE)
+  remotes::dev_package_deps(pkg$path, dependencies = "VignetteBuilder")
   message("Building ", pkg$package, " vignettes")
   tools::buildVignettes(dir = pkg$path, tangle = TRUE, clean = FALSE)
   devtools:::copy_vignettes(pkg, keep_md = FALSE)
   invisible(TRUE)
 }
 
+
 .update_drat <- function() {
-  path <- devtools::build(binary = FALSE, vignettes = FALSE)
-  drat::insertPackage(path, "~/Boulot/Mes_projets_de_recherche/R_packages/drat", commit = TRUE)
-  system("cd ~/Boulot/Mes_projets_de_recherche/R_packages/drat; git push") ## note: takes a while
+  path <- devtools::build(binary = FALSE, vignettes = TRUE)
+  drat::insertPackage(path, repodir = "~/Boulot/Mes_projets_de_recherche/R_packages/drat", commit = TRUE)
+  drat::pruneRepo(repopath = "~/Boulot/Mes_projets_de_recherche/R_packages/drat", remove = TRUE)
+  system("cd ~/Boulot/Mes_projets_de_recherche/R_packages/drat; git add .; git commit -m 'Pruning';  git push") ## note: takes a while
 }
